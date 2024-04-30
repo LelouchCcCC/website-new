24105a750994e31386cfb92f6edb816538653652
Yuhan <605286079@qq.com>
2024-04-30 15:36:57 -0400

    mock question data

---
+++ src/utils/mockLeetcode.js
@@ -0,0 +1,25 @@
+export const LC = [
+    {
+        id: "487312",
+        date: "--",
+        diff: "easy",
+        number: "1000",
+        title:"dnwuhudwq"
+    },
+    {
+        id: "487313232",
+        date: "--",
+        diff: "hard",
+        number: "12",
+        title:"ncjncjnhcuehhdsnchfqu n uehndjehfefe "
+    },
+    {
+        id: "10932",
+        date: "--",
+        diff: "medium",
+        number: "9734",
+        title:"n  nwkdnnd wnk ndj"
+    },
+]
+
+export default LC
